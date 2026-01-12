2b3e2e04fda8a1505b64a073894c47dd2aa172c8
khushijha24 <khushiujan2005@gmail.com>
2026-01-12 19:34:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/pro_sb_ecommerce/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pro_sb_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
