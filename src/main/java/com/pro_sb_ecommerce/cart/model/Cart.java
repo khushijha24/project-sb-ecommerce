f3f0013b41f2e434340d9e25b6b900006c8025ef
khushijha24 <khushiujan2005@gmail.com>
2026-02-11 20:46:07 +0530

    Order Module

--- src/main/java/com/pro_sb_ecommerce/cart/model/Cart.java
+++ src/main/java/com/pro_sb_ecommerce/cart/model/Cart.java
@@ -33,21 +33,4 @@ public class Cart {
             orphanRemoval = true
     )
     private List<CartItem> items = new ArrayList<>();
-
-//    @Column(nullable = false, updatable = false)
-//    private LocalDateTime createdAt;
-//
-//    @Column(nullable = false)
-//    private LocalDateTime updatedAt;
-//
-//    @PrePersist
-//    public void onCreate(){
-//        createdAt = LocalDateTime.now();
-//        updatedAt = LocalDateTime.now();
-//    }
-//
-//    @PreUpdate
-//    public void onUpdate(){
-//        updatedAt = LocalDateTime.now();
-//    }
 }
--- src/main/java/com/pro_sb_ecommerce/order/model/Order.java
+++ src/main/java/com/pro_sb_ecommerce/order/model/Order.java
@@ -1,11 +1,35 @@
 package com.pro_sb_ecommerce.order.model;
 
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
+import com.pro_sb_ecommerce.auth.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    LocalDateTime orderDate;
+
+    BigDecimal totalAmount;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> items = new ArrayList<>();
 }
